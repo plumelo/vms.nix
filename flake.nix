281a16f4558c6d102189a05c4c85c5c617481e71
Meghea Iulian <iulian.meghea@gmail.com>
2023-07-12 18:14:04 +0300

    chore(deps): upgrade

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   description = "Run NixOS VMs with qemu";
+  inputs.nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
   outputs = { self, nixpkgs }: {
     nixosModules.default = { config, lib, pkgs, ... }@args: {
       options = import ./lib/options.nix args;
